c06984818fe0856d3aa0d841f29d34096620a232
Philipp Fehrmann <philipp-09@live.de>
2016-11-16 11:05:25 +0100

    Added type comment

--- sign_detection/model/IdentifiedImage.py
+++ sign_detection/model/IdentifiedImage.py
@@ -3,7 +3,16 @@ from sign_detection.model.RegionOfInterest import RegionOfInterest
 
 
 class IdentifiedImage(Image):
+    def set_region_of_interests(self, value):
+        self._region_of_interests = value
+
+    def get_region_of_interests(self):
+        return self._region_of_interests
+
+    region_of_interests = property(get_region_of_interests, set_region_of_interests)
+
     def __init__(self, path_to_image, region_of_interests):
+        # type: (str, list(RegionOfInterest)) -> IdentifiedImage
         """
 
         :param path_to_image:
@@ -16,10 +25,3 @@ class IdentifiedImage(Image):
         rois.extend(region_of_interests)
         self.region_of_interests = rois
 
-    def set_region_of_interests(self, value):
-        self._region_of_interests = value
-
-    def get_region_of_interests(self):
-        return self._region_of_interests
-
-    region_of_interests = property(get_region_of_interests, set_region_of_interests)
