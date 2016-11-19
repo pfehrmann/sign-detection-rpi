d4435ee68ea0e7aceca9abbbf49ab810bea3c338
Philipp Fehrmann <philipp-09@live.de>
2016-11-19 19:18:13 +0100

    Added function to find all overlapping regions

--- sign_detection/model/IdentifiedImage.py
+++ sign_detection/model/IdentifiedImage.py
@@ -3,10 +3,23 @@ from sign_detection.model.RegionOfInterest import RegionOfInterest
 
 
 class IdentifiedImage(Image):
-    def set_region_of_interests(self, value):
-        self._region_of_interests = value
+    def set_region_of_interests(self, rois):
+        """
+        Set the regions of interest
+
+        :param rois: a list containing regions of interest
+        :return: None
+
+        :type rois: list[RegionOfInterest]
+        """
+        self._region_of_interests = rois
 
     def get_region_of_interests(self):
+        """
+
+        :return: All teh regions of interest
+        :returns: list[RegionOfInterest]
+        """
         return self._region_of_interests
 
     region_of_interests = property(get_region_of_interests, set_region_of_interests)
@@ -25,3 +38,19 @@ class IdentifiedImage(Image):
         rois.extend(region_of_interests)
         self.region_of_interests = rois
 
+    def get_overlapping_regions(self, region_of_interest, overlap=0.85):
+        """
+        Get all Regions Of Interest, that overlap with the given ROI
+        :param region_of_interest: The ROI to check
+        :param overlap: The minimal percentage of overlap. 0 will return every region, 1 will return only regions that overlap completely.
+        :return: A list of all overlapping regoins
+        :returns: list[RegionOfInterest]
+        :type region_of_interest: RegionOfInterest
+        :type overlap: float
+        """
+
+        regions = []
+        for roi in self.region_of_interests:
+            if roi.getOverlap(region_of_interest) > overlap:
+                regions.append(roi)
+        return regions
