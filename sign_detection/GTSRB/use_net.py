fa1df9a28c9aa110f9c272a324cb4ee9e81f7e41
Philipp Fehrmann <philipp-09@live.de>
2016-12-01 10:29:31 +0100

    Added timing and output

--- sign_detection/GTSRB/use_net.py
+++ sign_detection/GTSRB/use_net.py
@@ -1,9 +1,9 @@
 import caffe
 import argparse
+import timeit
 
 
-def identify_image(model, weights, image_path):
-    # caffe.set_device(0)
+def load_net(model, weights):
     caffe.set_mode_cpu()
 
     net = caffe.Net(model, weights, caffe.TEST)
@@ -13,11 +13,15 @@ def identify_image(model, weights, image_path):
     transformer.set_transpose('data', (2, 0, 1))
     transformer.set_channel_swap('data', (2, 1, 0))
     transformer.set_raw_scale('data', 255.0)
+    return net, transformer
 
+def load_image(image_path, net, transformer):
     # load the image in the data layer
     im = caffe.io.load_image(image_path)
     net.blobs['data'].data[...] = transformer.preprocess('data', im)
+    return net
 
+def compute(net):
     # compute
     out = net.forward()
 
@@ -30,8 +34,8 @@ def parse_arguments():
     # Create the parser
     parser = argparse.ArgumentParser(description='Use a trained net to identify images')
     parser.add_argument('image', type=str, nargs='+', help='An image to identify')
-    parser.add_argument('-m', '--model', type=str, default='model.prototxt', help='The model to use (.prototxt)')
-    parser.add_argument('-w', '--weights', type=str, default='weights.caffemodel',
+    parser.add_argument('-m', '--model', type=str, default='/home/pi/development/nets/quadruple_nin_deploy.prototxt', help='The model to use (.prototxt)')
+    parser.add_argument('-w', '--weights', type=str, default='/home/pi/development/nets/quadruple_nin_iter_45000.caffemodel',
                         help='The weights to use (trained net / .caffemodel)')
 
     # Read the input arguments
@@ -42,7 +46,16 @@ def parse_arguments():
         print 'Only one image allowed for now. Ignoring others.'
 
     # pass arguments and start identifying
-    identify_image(args.model, args.weights, args.image[0])
+    net, transformer = load_net(args.model, args.weights)
+    net = load_image(args.image[0], net, transformer)
 
+    start = timeit.default_timer()
+
+    category = compute(net)
+
+    stop = timeit.default_timer()
+    
+    print("Time:  " + str(stop - start))
+    print "Category: " + str(category)
 
 parse_arguments()
