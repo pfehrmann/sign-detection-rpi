42b091426664aa2918faf580ef4128434018ab42
Philipp Fehrmann <philipp-09@live.de>
2016-12-07 09:23:35 +0100

    Bugfixes and comments

--- sign_detection/GTSDB/SlidingWindow/batchloader.py
+++ sign_detection/GTSDB/SlidingWindow/batchloader.py
@@ -3,6 +3,7 @@ import random
 from os import path as osp
 from random import shuffle
 
+import gc
 import numpy as np
 from PIL import Image
 
@@ -21,6 +22,13 @@ class BatchLoader(object):
     """
 
     def __init__(self, params, num, fraction):
+        """
+        Initialize the batch loader. This already reads the list of images but generates no windows.
+        :param params: The parameter dictionary containing the values batch_size, gtsdb_root and window_size
+        :param num: The number of windows to go through before filtering some out. Note that the higher this number is,
+                    the more random the results are but also more RAM will be used.
+        :param fraction: The percentage of images with signs (eg. fraction == 0.3: signs: 100, no signs: 233)
+        """
         self.batch_size = params['batch_size']
         self.gtsdb_root = params['gtsdb_root']
         self.window_size = params['window_size']
@@ -32,42 +40,67 @@ class BatchLoader(object):
 
         self.images = get_images_and_regions(self.gtsdb_root)
 
-        print "BatchLoader initialized with {} images".format(
-            len(self.images))
+        print "BatchLoader initialized with {} images, {}% of each collection are signs".format(
+            len(self.images), fraction)
 
     def next_window(self):
+        """
+        Get the next window. The windows are generated before, so it can take a while until this function returns
+        :return: A new window
+        :returns: list[list[list[float]]], int
+        """
         try:
             return self._windows.pop()
         except:
-            self.collect_windows()
-            return self._windows.pop()
+            self.__collect_windows()
 
-    def collect_windows(self):
+            # Make sure that all the unused images are removed from RAM
+            gc.collect()
+
+            # In case that no windows were generated, retry everything
+            return self.next_window()
+
+    def __collect_windows(self):
+        """
+        Generate a list of windows from windows
+        :return: None
+        :returns: None
+        """
         signs = []
         no_signs = []
 
         for i in range(0, self.num, 1):
             image, label = self.__load_next_window()
-            if label[0] == 1:
-                no_signs.append((image, label))
-            else:
+            if label == 1:
                 signs.append((image, label))
+            else:
+                no_signs.append((image, label))
 
-        no_sign_count = max(len(signs) * self.fraction, len(no_signs))
+        no_sign_count = max(0, min(len(signs) * self.fraction - len(signs), len(no_signs)))
         random.shuffle(no_signs)
 
-        # create result array
+        # create and shuffle result array
         result = []
         result.extend(signs)
         result.extend(no_signs[0:int(no_sign_count)])
         random.shuffle(result)
         self._windows = result
 
+        # some debug info
+        print "Collected {} images. Stored {} images, {} of these contain signs, {} of these contain no signs. " \
+              "Overall {} images contain no signs.".format(
+               self.num, len(result), len(signs), no_sign_count, len(no_signs))
+
     def __load_next_window(self):
+        """
+        load the next window. If the current image contains no more windows, loads the next window.
+        :return: A window and the corresponding list of regions of interest
+        :returns: list[list[list[float]]], int
+        """
         try:
             image_raw, current_window = self._sliding_window.next()
         except:
-            self._image, image_data = self.load_next_image()
+            self._image, image_data = self.__load_next_image()
             self._sliding_window = ScalingSlidingWindow(image_data, self.window_size, 1,
                                                         zoom_factor=lambda x: 1 / (x + 1))
             image_raw, current_window = self._sliding_window.next()
@@ -76,15 +109,18 @@ class BatchLoader(object):
         regions = self._image.get_overlapping_regions(current_window, 0.85)
 
         # Load and prepare ground truth
-        label = np.zeros(1).astype(np.float32)
+        label = 0
         if len(regions) == 1:
-            label[0] = 1
+            label = 1
 
         return image_raw, label
 
-    def load_next_image(self):
+    def __load_next_image(self):
         """
-        Load the next image in a batch.
+        Load the next image.
+
+        :return: Returns the next labeled image and the raw image data
+        :returns: IdentifiedImage, list[list[list[float]]]
         """
 
         # Did we finish an epoch?
@@ -101,12 +137,21 @@ class BatchLoader(object):
 
 
 def get_images_and_regions(gtsdb_root):
+    """
+    Generates a list of images in the GTSDB contest. Only a maximum of 600 images are used. This is necessary, to also
+    be able to use just the training data set
+
+    :param gtsdb_root: The root of image folder. In this folder all the images have to reside.
+    :return: returns a list of identified images. These are already labeled
+    :returns: list[IdentifiedImage]
+    :type gtsdb_root: str
+    """
     # get list of image indexes.
     duplicated_images = []  # images
     with open(gtsdb_root + "/gt.txt") as gtFile:
-        gtReader = csv.reader(gtFile, delimiter=';')  # csv parser for annotations file
+        gt_reader = csv.reader(gtFile, delimiter=';')  # csv parser for annotations file
         # loop over all images in current annotations file
-        for row in gtReader:
+        for row in gt_reader:
             path_to_image = gtsdb_root + "/" + row[0]
 
             # find size of image
@@ -116,6 +161,7 @@ def get_images_and_regions(gtsdb_root):
 
     # fill in all the images without a region of interest
     # there are images ranging from 00000 to 00599
+    # higher numbers are ignored.
     for i in range(0, 599):
         found = False
         for image in duplicated_images:
