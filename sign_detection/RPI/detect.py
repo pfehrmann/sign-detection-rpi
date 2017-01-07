054f4bc79dd126186767c9e8ce0193ce0cd96336
Philipp Fehrmann <philipp-09@live.de>
2017-01-07 20:49:04 +0100

    Added detection file

---
+++ sign_detection/RPI/detect.py
@@ -0,0 +1,65 @@
+import picamera
+import picamera.array
+import cv2
+
+import sign_detection.GTSDB.ActivationMapBoundingBoxes.use_net as un
+from time import time
+
+
+def identify_regions(save=False, display=True):
+    # initialize caffe
+    un.setup_device(gpu=False)
+
+    if save:
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('output.avi', fourcc, 2.5, (640, 480))
+
+    # Setup the net and transformer
+    net = un.load_net("../GTSDB/ActivationMapBoundingBoxes/mini_net/deploy.prototxt", "../GTSDB/ActivationMapBoundingBoxes/mini_net/weights.caffemodel")
+
+    # setup the detector
+    detector = un.Detector(net, minimum=0.9999, use_global_max=False, threshold_factor=0.75, draw_results=False,
+                           zoom=[1], area_threshold_min=2000, area_threshold_max=30000, activation_layer="activation",
+                           out_layer="softmax", display_activation=False, blur_radius=1, size_factor=0.4)
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+
+        while True:
+            start = time()
+
+            # capture the image
+            output = picamera.array.PiRGBArray(camera)
+            camera.capture(output, 'rgb', use_video_port=False)
+            img = output.array
+
+            # pass the image through the net
+            rois, unfiltered = detector.identify_regions_from_image(img, img)
+
+            end = time()
+
+            # Show the regions
+            un.draw_regions(unfiltered, img, (0, 255, 0))
+            un.draw_regions(rois, img, (0, 0, 255), print_class=True)
+            cv2.putText(img, "{} fps".format(1.0/(end-start)), (5, img.shape[0] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+
+            if display:
+                cv2.imshow("Detection", img)
+
+            if save:
+                out.write(img)
+
+            # Exit with the escape key
+            key = cv2.waitKey(10)
+            if key == 27:
+                break
+
+    # clean up
+    cv2.destroyAllWindows()
+
+    if save:
+        out.release()
+
+
+if __name__ == '__main__':
+    identify_regions(save=False, display=True)
