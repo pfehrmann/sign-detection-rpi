78cde46c865b262d9b570e6278fcc881e6c19530
Philipp Fehrmann <philipp-09@live.de>
2017-03-08 20:50:27 +0100

    Bug fixes

--- sign_detection/RPI/multi_core_detection.py
+++ sign_detection/RPI/multi_core_detection.py
@@ -3,8 +3,8 @@ caffe_path = "/home/pi/development/caffe/build/install/python"
 if not caffe_path in sys.path:
     sys.path.append(caffe_path)
 
-from sign_detection.GTSDB.image_sources import PiCameraImageSource
 import sign_detection.EV3.EV3 as ev3
+from sign_detection.RPI.PiCameraImageSource import PiCameraImageSource
 from sign_detection.GTSDB.roi_result_handlers import EV3Handler, ConsoleHandler
 
 import time
@@ -12,7 +12,7 @@ import time
 from sign_detection.GTSDB.multi_processor_detection import Master
 import sign_detection.GTSDB.ActivationMapBoundingBoxes.use_net as un
 
-num_workers = 5
+num_workers = 10
 
 
 def create_detector():
@@ -26,7 +26,7 @@ def create_detector():
     un.setup_device(gpu=False)
     detector = un.Detector(net,
                            minimum=0.9999,
-                           use_global_max=True,
+                           use_global_max=False,
                            threshold_factor=0.75,
                            draw_results=False,
                            zoom=[1],
@@ -36,10 +36,10 @@ def create_detector():
                            out_layer="softmax",
                            display_activation=False,
                            blur_radius=1,
-                           size_factor=0.4,
+                           size_factor=0.1,
                            max_overlap=0.5,
                            faster_rcnn=True,
-                           modify_average_value=False,
+                           modify_average_value=True,
                            average_value=60)
     return detector
 
@@ -52,7 +52,6 @@ def test():
     global num_workers
     image_source = PiCameraImageSource()
     master = Master(create_detector, image_source, num_workers)
-    master.register_roi_result_handler(EV3Handler())
     my_ev3 = ev3.EV3(protocol=ev3.USB, host='00:16:53:47:92:46')
     my_ev3.sync_mode = ev3.ASYNC
     master.register_roi_result_handler(EV3Handler(my_ev3))
