feb989741da55ab0c0a479adeb963ce261ac8569
Philipp Fehrmann <philipp-09@live.de>
2017-03-08 20:54:43 +0100

    Removed dir listing, added image displaying

--- sign_detection/EV3/images.py
+++ sign_detection/EV3/images.py
@@ -1,23 +1,21 @@
 import sign_detection.EV3.EV3 as ev3
-import struct
 
 
-def get_folder(dir, my_ev3):
-    directory = dir
+def show_image(image_path, my_ev3):
     ops = b''.join([
-        ev3.opFile,
-        ev3.GET_FOLDERS,
-        ev3.LCS(directory),
-        ev3.GVX(0)
+        ev3.opUI_Draw,
+        ev3.TOPLINE,
+        ev3.LCX(0),  # ENABLE
+        ev3.opUI_Draw,
+        ev3.BMPFILE,
+        ev3.LCX(1),  # COLOR
+        ev3.LCX(0),  # X0
+        ev3.LCX(0),  # Y0
+        ev3.LCS(image_path),  # NAME
+        ev3.opUI_Draw,
+        ev3.UPDATE
     ])
-    reply = my_ev3.send_direct_cmd(ops, global_mem=1)
-    num = struct.unpack('<B', reply[5:])[0]
-    print(
-        "Directory '{}' has {} subdirectories".format(
-            directory,
-            num
-        )
-    )
+    reply = my_ev3.send_direct_cmd(ops)
 
 if __name__ == "__main__":
     myEv3 = ev3.EV3(protocol=ev3.USB, host='00:16:53:47:92:46')
