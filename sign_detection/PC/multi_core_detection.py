c8a1a4d33450ffbbebbe0708d71a7099bb9806c9
Philipp Fehrmann <philipp-09@live.de>
2017-03-16 10:31:43 +0100

    Tweaked settings

--- sign_detection/PC/multi_core_detection.py
+++ sign_detection/PC/multi_core_detection.py
@@ -2,8 +2,8 @@ import time
 
 import sign_detection.GTSDB.multi_processor_detection as mpd
 import sign_detection.GTSDB.ActivationMapBoundingBoxes.use_net as un
-from sign_detection.GTSDB.image_sources import CV2CameraImageSource
-from sign_detection.GTSDB.roi_result_handlers import ViewHandler
+from sign_detection.GTSDB.image_sources import CV2CameraImageSource, CV2VideoImageSource
+from sign_detection.GTSDB.roi_result_handlers import ViewHandler, ConsoleHandler
 
 num_workers = 4
 
@@ -14,26 +14,27 @@ def create_detector():
     :return: A Detector
     :returns: sign_detection.GTSDB.Detector_Base
     """
-    net = un.load_net("../GTSDB/ActivationMapBoundingBoxes/mini_net/deploy.prototxt",
-                      "../GTSDB/ActivationMapBoundingBoxes/mini_net/weights.caffemodel")
+    net = un.load_net("../GTSDB/ActivationMapBoundingBoxes/mini_net_aug_scale/deploy.prototxt",
+                      "../GTSDB/ActivationMapBoundingBoxes/mini_net_aug_scale/weights.caffemodel")
     un.setup_device(gpu=True)
     detector = un.Detector(net,
-                                minimum=0.9999,
-                                use_global_max=True,
-                                threshold_factor=0.75,
-                                draw_results=False,
-                                zoom=[1],
-                                area_threshold_min=1000,
-                                area_threshold_max=500000,
-                                activation_layer="activation",
-                                out_layer="softmax",
-                                display_activation=False,
-                                blur_radius=1,
-                                size_factor=0.4,
-                                max_overlap=0.5,
-                                faster_rcnn=True,
-                                modify_average_value=False,
-                                average_value=60)
+                           minimum=0.25,
+                           use_global_max=False,
+                           threshold_factor=0.75,
+                           draw_results=False,
+                           zoom=[1, 2],
+                           area_threshold_min=1000,
+                           area_threshold_max=500000,
+                           activation_layer="activation",
+                           out_layer="softmax",
+                           global_pooling_layer="conv3",
+                           display_activation=False,
+                           blur_radius=1,
+                           size_factor=0.5,
+                           max_overlap=0.5,
+                           faster_rcnn=True,
+                           modify_average_value=True,
+                           average_value=100)
     return detector
 
 
@@ -44,8 +45,10 @@ def test():
     """
     global num_workers
     image_source = CV2CameraImageSource()
-    master = mpd.Master(create_detector, image_source, num_workers)
+    # image_source = CV2VideoImageSource(rotate_angle=180)
+    master = mpd.Master(create_detector, image_source, num_workers=num_workers, method=mpd.CONSTANT_FPS)
     master.register_roi_result_handler(ViewHandler(num_workers))
+    master.register_roi_result_handler(ConsoleHandler(num_workers))
     master.start()
     try:
         while 1:
