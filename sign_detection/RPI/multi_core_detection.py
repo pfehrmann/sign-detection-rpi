668d16c779b1f5021a63d79d097e56fd3f49ee4b
Philipp Fehrmann <philipp-09@live.de>
2017-03-16 10:31:02 +0100

    Tweaked settings and changed import names

--- sign_detection/RPI/multi_core_detection.py
+++ sign_detection/RPI/multi_core_detection.py
@@ -5,14 +5,14 @@ if not caffe_path in sys.path:
 
 import sign_detection.EV3.EV3 as ev3
 from sign_detection.RPI.PiCameraImageSource import PiCameraImageSource
-from sign_detection.GTSDB.roi_result_handlers import EV3Handler, ConsoleHandler
+from sign_detection.GTSDB.roi_result_handlers import EV3Handler, ConsoleHandler, ViewHandler
 
 import time
 
-from sign_detection.GTSDB.multi_processor_detection import Master
+import sign_detection.GTSDB.multi_processor_detection as mpd
 import sign_detection.GTSDB.ActivationMapBoundingBoxes.use_net as un
 
-num_workers = 10
+num_workers = 5
 
 
 def create_detector():
@@ -29,18 +29,18 @@ def create_detector():
                            use_global_max=False,
                            threshold_factor=0.75,
                            draw_results=False,
-                           zoom=[1],
-                           area_threshold_min=1000,
+                           zoom=[1, 2],
+                           area_threshold_min=100,
                            area_threshold_max=500000,
                            activation_layer="activation",
                            out_layer="softmax",
                            display_activation=False,
                            blur_radius=1,
                            size_factor=0.1,
-                           max_overlap=0.5,
+                           max_overlap=0.35,
                            faster_rcnn=True,
-                           modify_average_value=True,
-                           average_value=60)
+                           modify_average_value=False,
+                           average_value=100)
     return detector
 
 
@@ -50,12 +50,13 @@ def test():
     :return:
     """
     global num_workers
-    image_source = PiCameraImageSource()
-    master = Master(create_detector, image_source, num_workers)
+    image_source = PiCameraImageSource(average_value=100)
+    master = mpd.Master(create_detector, image_source, num_workers=num_workers, method=mpd.BURST)
     my_ev3 = ev3.EV3(protocol=ev3.USB, host='00:16:53:47:92:46')
     my_ev3.sync_mode = ev3.ASYNC
     master.register_roi_result_handler(EV3Handler(my_ev3))
-    master.register_roi_result_handler(ConsoleHandler(num_workers))
+    master.register_roi_result_handler(ConsoleHandler(num_workers, num_timestamps=num_workers * 10))
+    master.register_roi_result_handler(ViewHandler(num_workers))
     master.start()
     try:
         while 1:
