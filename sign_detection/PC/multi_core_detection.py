9870a630d792a0176bd59fbadda073553197f4f2
Philipp Fehrmann <philipp-09@live.de>
2017-04-12 08:25:18 +0200

    Using net with junk classes

--- sign_detection/PC/multi_core_detection.py
+++ sign_detection/PC/multi_core_detection.py
@@ -3,7 +3,7 @@ import time
 import sign_detection.GTSDB.multi_processor_detection as mpd
 import sign_detection.GTSDB.ActivationMapBoundingBoxes.use_net as un
 from sign_detection.GTSDB.image_sources import CV2CameraImageSource, CV2VideoImageSource
-from sign_detection.GTSDB.roi_result_handlers import ViewHandler, ConsoleHandler
+from sign_detection.GTSDB.roi_result_handlers import ViewHandler, ConsoleHandler, JunkRemover
 
 num_workers = 4
 
@@ -14,11 +14,11 @@ def create_detector():
     :return: A Detector
     :returns: sign_detection.GTSDB.Detector_Base
     """
-    net = un.load_net("../GTSDB/ActivationMapBoundingBoxes/mini_net_aug_scale/deploy.prototxt",
-                      "../GTSDB/ActivationMapBoundingBoxes/mini_net_aug_scale/weights.caffemodel")
+    net = un.load_net("../GTSDB/ActivationMapBoundingBoxes/mini_net_much_junk/deploy.prototxt",
+                      "../GTSDB/ActivationMapBoundingBoxes/mini_net_much_junk/weights.caffemodel")
     un.setup_device(gpu=True)
     detector = un.Detector(net,
-                           minimum=0.25,
+                           minimum=0.80,
                            use_global_max=False,
                            threshold_factor=0.75,
                            draw_results=False,
@@ -27,14 +27,14 @@ def create_detector():
                            area_threshold_max=500000,
                            activation_layer="activation",
                            out_layer="softmax",
-                           global_pooling_layer="conv3",
+                           global_pooling_layer="pool1",
                            display_activation=False,
                            blur_radius=1,
                            size_factor=0.5,
                            max_overlap=0.5,
                            faster_rcnn=True,
                            modify_average_value=True,
-                           average_value=100)
+                           average_value=20)
     return detector
 
 
@@ -44,11 +44,14 @@ def test():
     :return:
     """
     global num_workers
-    image_source = CV2CameraImageSource()
-    # image_source = CV2VideoImageSource(rotate_angle=180)
-    master = mpd.Master(create_detector, image_source, num_workers=num_workers, method=mpd.CONSTANT_FPS)
-    master.register_roi_result_handler(ViewHandler(num_workers))
-    master.register_roi_result_handler(ConsoleHandler(num_workers))
+    # image_source = CV2CameraImageSource()
+    image_source = CV2VideoImageSource(rotate_angle=180, saturation_factor=1,
+                                       image_source="C:/development/Videos/2017-03-05 23.33.33.437823.h264")
+    master = mpd.Master(create_detector, image_source, num_workers=num_workers, method=mpd.BURST)
+    view_handler = ViewHandler(num_workers, "Filtered")
+    master.register_roi_result_handler(JunkRemover(view_handler, [43]))
+    master.register_roi_result_handler(ViewHandler(num_workers, "Unfiltered"))
+    master.register_roi_result_handler(JunkRemover(ConsoleHandler(num_workers)))
     master.start()
     try:
         while 1:
